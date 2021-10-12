8eebbf8cd9dd9ee54db782cae2900bab58bde066
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-10-11 22:02:47 -0400

    q_1_1_is_unique added tests

--- chapter_1_arrays_and_string/q_1_1_is_unique.py
+++ chapter_1_arrays_and_string/q_1_1_is_unique.py
@@ -1,9 +1,10 @@
 """
 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 """
+import unittest
 
 
-def checK_unique_chacters_in_string(target_string: str):
+def check_unique_characters_in_string(target_string: str = "") -> bool:
     character_set = set()
     for character in list(target_string):
         if character in character_set:
@@ -13,8 +14,23 @@ def checK_unique_chacters_in_string(target_string: str):
     return True
 
 
-print(checK_unique_chacters_in_string("test"))
-print(checK_unique_chacters_in_string("tag"))
-print(checK_unique_chacters_in_string("trouble"))
+class TestUniqueCharacters(unittest.TestCase):
+    def test_return_type(self):
+        self.assertEqual(type(check_unique_characters_in_string("test")), bool)
+
+    def test_unique_character_false_case(self):
+        self.assertEqual(check_unique_characters_in_string("test"), False)
+
+    def test_unique_character_true_case(self):
+        self.assertEqual(check_unique_characters_in_string("another"), True)
+
+    def test_unique_character_empty_string(self):
+        self.assertEqual(check_unique_characters_in_string(""), True)
+
+    def test_unique_character_no_string(self):
+        self.assertEqual(check_unique_characters_in_string(), True)
 
 
+if __name__ == '__main__':
+    unittest.main()
+    
