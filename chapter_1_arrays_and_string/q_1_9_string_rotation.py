6b08d7e8a5c3dad83e770bcad0bd6e8fee4a4068
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-10-18 19:22:11 -0400

    q_1_9_string_rotation.py

---
+++ chapter_1_arrays_and_string/q_1_9_string_rotation.py
@@ -0,0 +1,62 @@
+"""
+Assume you have a method isSubstring which checks if one is a substring of another. Give two strings
+s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (eg "waterbottle"
+is a rotation of "erbottlewat")
+"""
+import unittest
+
+
+def is_substring(string_one: str = "", string_two: str = "") -> bool:
+    string_one_counter = 0
+    string_two_counter = 0
+    for s2_index, character in enumerate(string_two):
+        string_two_counter = s2_index
+        while string_one[string_one_counter] == string_two[string_two_counter]:
+            string_two_counter = string_two_counter + 1 if string_two_counter < len(string_two) - 1 else 0
+            if string_one_counter < len(string_one) - 1:
+                string_one_counter += 1
+            else:
+                return True
+    return False
+
+
+class TestIsSubstring(unittest.TestCase):
+    def setUp(self) -> None:
+        self.one_letter = "a"
+        self.one_letter_rotated = "a"
+        self.two_letter = "ab"
+        self.two_letter_rotated = "ba"
+        self.use_case_one = "waterbottle"
+        self.use_case_one_rotated_one = "erbottlewat"
+        self.use_case_one_rotated_two = "bottlewater"
+        self.use_case_one_rotated_three = "lewaterbott"
+        self.use_case_one_wrong_one = "bttleowater"
+        self.use_case_one_wrong_two = "obottle"
+
+    def test_return_type(self):
+        self.assertEqual(bool, type(is_substring()))
+
+    def test_one_letter(self):
+        self.assertTrue(is_substring(self.one_letter, self.one_letter_rotated))
+
+    def test_two_letter(self):
+        self.assertTrue(is_substring(self.two_letter, self.two_letter_rotated))
+
+    def test_use_case_one(self):
+        self.assertTrue(is_substring(self.use_case_one, self.use_case_one_rotated_one))
+
+    def test_use_case_two(self):
+        self.assertTrue(is_substring(self.use_case_one, self.use_case_one_rotated_two))
+
+    def test_use_case_three(self):
+        self.assertTrue(is_substring(self.use_case_one, self.use_case_one_rotated_three))
+
+    def test_use_case_four(self):
+        self.assertFalse(is_substring(self.use_case_one, self.use_case_one_wrong_one))
+
+    def test_use_case_five(self):
+        self.assertFalse(is_substring(self.use_case_one, self.use_case_one_wrong_two))
+
+
+if __name__ == '__main__':
+    unittest.main()
