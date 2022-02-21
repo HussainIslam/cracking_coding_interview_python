03b0c34c3ca848e7b97bed27c52f9f9bb98f6aa2
Hussain <m.hussainul.islam@gmail.com>
2022-02-21 13:25:02 -0500

    - Add solution using additional data structure for q_1_1_is_unique.py

--- chapter_1_arrays_and_string/q_1_1_is_unique.py
+++ chapter_1_arrays_and_string/q_1_1_is_unique.py
@@ -1,10 +1,11 @@
 """
-Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data
+structures?
 """
 import unittest
 
 
-def check_unique_characters_in_string(target_string: str = "") -> bool:
+def check_unique_characters_without_additional_data_structure(target_string: str = "") -> bool:
     length = len(target_string)
     for i in range(length):
         for j in range(i + 1, length):
@@ -13,21 +14,35 @@ def check_unique_characters_in_string(target_string: str = "") -> bool:
     return True
 
 
+def check_unique_characters(target_string: str) -> bool:
+    # Assumes the characters set is ASCII and total number of unique characters is 128
+    # Alternatively, we can assume Extended ASCII and 256 unique characters
+    if len(target_string) > 128:
+        return False
+    character_set = [False] * 128
+    for character in target_string:
+        ascii_code = ord(character)
+        if character_set[ascii_code]:
+            return False
+        character_set[ascii_code] = True
+    return True
+
+
 class TestUniqueCharacters(unittest.TestCase):
     def test_return_type(self):
-        self.assertEqual(type(check_unique_characters_in_string("test")), bool)
+        self.assertEqual(type(check_unique_characters_without_additional_data_structure("test")), bool)
 
     def test_unique_character_false_case(self):
-        self.assertEqual(check_unique_characters_in_string("test"), False)
+        self.assertEqual(check_unique_characters_without_additional_data_structure("test"), False)
 
     def test_unique_character_true_case(self):
-        self.assertEqual(check_unique_characters_in_string("another"), True)
+        self.assertEqual(check_unique_characters_without_additional_data_structure("another"), True)
 
     def test_unique_character_empty_string(self):
-        self.assertEqual(check_unique_characters_in_string(""), True)
+        self.assertEqual(check_unique_characters_without_additional_data_structure(""), True)
 
     def test_unique_character_no_string(self):
-        self.assertEqual(check_unique_characters_in_string(), True)
+        self.assertEqual(check_unique_characters_without_additional_data_structure(), True)
 
 
 if __name__ == '__main__':
