de5f861f7b34d8399c3ad2812ac996cacf8dba4a
Hussain <m.hussainul.islam@gmail.com>
2022-02-25 20:37:11 -0500

    q_1_4_palindrome_permutation.py - Refactor test cases - Refactor previous solution - Add solution based on ASCII character counter

--- chapter_1_arrays_and_string/q_1_4_palindrome_permutation.py
+++ chapter_1_arrays_and_string/q_1_4_palindrome_permutation.py
@@ -8,37 +8,67 @@ Example:
     Output: True (permutation: taco cat, atco cta, etc
 """
 import unittest
+import string
 
 
 def is_palindrome_permutation(input_string: str = "") -> bool:
     character_counter = {}
     input_string = input_string.replace(" ", "").lower()
-    if len(input_string) > 1:
-        for character in list(input_string):
-            character_counter[character] = 1 if character not in character_counter.keys() else character_counter[character] + 1
-        characters_without_pair = 0
-        for character, frequency in character_counter.items():
-            if frequency % 2 == 1:
-                characters_without_pair += 1
-        return characters_without_pair <= 1
-    return False
+    for character in list(input_string):
+        character_counter[character] = 1 if character not in character_counter.keys() else character_counter[character] + 1
+    characters_without_pair = 0
+    for character, frequency in character_counter.items():
+        if frequency % 2 == 1:
+            characters_without_pair += 1
+    return characters_without_pair <= 1
+
+
+def is_palindrome_permutation_counter(input_string: str = "") -> bool:
+    character_list = [0] * 26
+    valid_characters = 0
+    for character in input_string:
+        if 65 <= ord(character) <= 90:
+            character_list[ord(character) - 65] += 1
+            valid_characters += 1
+        if 97 <= ord(character) <= 122:
+            character_list[ord(character) - 97] += 1
+            valid_characters += 1
+    odd_counter = 0
+    for character_counter in character_list:
+        if character_counter % 2:
+            odd_counter += 1
+            if odd_counter > 1:
+                return False
+    return True
 
 
 class TestPalindromePermutation(unittest.TestCase):
-    def test_return_type(self):
-        self.assertEqual(type(is_palindrome_permutation()), bool)
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.data_sets = [
+            ("Tact Coa", True),
+            ("tattarrattat", True),
+            ("nothing", False),
+            ("aabcb", True),
+            ("aabccb", True),
+            ("abcbd", False),
+            ("bc", False),
+            ("abcAb", True),
+            ("A", True),
+            ("1221", True),
+            ("abcdef", False)
+        ]
+        cls.methods = [
+            'is_palindrome_permutation',
+            'is_palindrome_permutation_counter',
+        ]
 
     def test_use_cases(self):
-        self.assertEqual(is_palindrome_permutation("Tact Coa"), True)
-        self.assertEqual(is_palindrome_permutation("tattarrattat"), True)
-        self.assertEqual(is_palindrome_permutation("nothing"), False)
-        self.assertEqual(is_palindrome_permutation("aabcb"), True)
-        self.assertEqual(is_palindrome_permutation("aabccb"), True)
-        self.assertEqual(is_palindrome_permutation("abcbd"), False)
-        self.assertEqual(is_palindrome_permutation("bc"), False)
-        self.assertEqual(is_palindrome_permutation("abcAb"), True)
-        self.assertEqual(is_palindrome_permutation("A"), False)
-        self.assertEqual(is_palindrome_permutation("1221"), True)
+        for method in self.methods:
+            print(f"--------------------- TESTING: {method} ---------------------")
+            for data_set in self.data_sets:
+                print(f"Test: {data_set[0]} == {data_set[1]}")
+                assert globals()[method](data_set[0]) == data_set[1]
 
 
 if __name__ == '__main__':
